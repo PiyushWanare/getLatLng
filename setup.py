ff6ec4b752f82c1ef750fc840a2d74322e3abaf3
PiyushW <piyushwankhade01@gmail.com>
2016-12-30 12:49:20 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='getLatLng',
+      version='1.0.0',
+      description='Directory to get latitude and longitute values of particular city.',
+      url='https://github.com/36rahu/pincode_directory',
+      author='Piyush Wanare',
+      author_email='piyushwanare24@gmail.com',
+      license='MIT',
+      zip_safe=False,
+      packages=['getLatLng'],
+      package_dir={'getLatLng': 'getLatLng/'},
+    )
